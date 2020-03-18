06efd72ee7a49c24b56a4afedaad67a358aacb4d
Michał Kruszewski <kruszewski2@gmail.com>
2020-03-18 23:41:05 +0100

    custom user model added

--- simple_authentication/authentication/models.py
+++ simple_authentication/authentication/models.py
@@ -1,9 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 
 
-class UserDB(User):
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    is_admin = models.BooleanField(default=False)
+
     some_additional_property = models.CharField(max_length=200)
 
+    # objects = MyUserManager()
+    USERNAME_FIELD = username
+    REQUIRED_FIELDS = ['username', 'password']
+
+    def __str__(self):
+        return self.USERNAME_FIELD
+
+    @property
+    def is_admin(self):
+        return self.is_admin
